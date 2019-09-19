9dfb12179538eb4c78afeed2e4ad6bdf8ae2c677
Nikola <nilic993@gmail.com>
2019-09-19 12:09:26 +0200

    Added input request, new images

--- watershed-test.py
+++ watershed-test.py
@@ -1,19 +1,23 @@
 import cv2
 import numpy as np
 import os
+import sys
 from matplotlib import cm
 
 # Inputs
 
 img_folder = "./img/"
 res_s_folder = "./res_s/"
+load_im = 'path'
 
-if not os.path.exists(img_folder): # If directories missing
+if not os.path.exists(img_folder): # If directories missing + image path
     os.makedirs(img_folder)
 if not os.path.exists(res_s_folder):
     os.makedirs(res_s_folder)
+while not os.path.exists(img_folder+load_im):
+    load_im = input("Enter filename (example: 1.jpg): ")
 
-img = cv2.imread('img/test.jpg') # Input image
+img = cv2.imread('img/'+load_im) # Input image
 img_copy = np.copy(img)
 
 marker_image = np.zeros(img.shape[:2],dtype=np.int32)
@@ -21,6 +25,7 @@ segments = np.zeros(img.shape,dtype=np.uint8) # Empty
 
 n_markers = 10
 current_marker = 1
+s = 0
 
 # Markers
 markers_updated = False
@@ -28,25 +33,25 @@ markers_updated = False
 # Callback
 
 def mouse_callback(event,x,y,flags,param):
-	global markers_updated
-	
-	if event == cv2.EVENT_LBUTTONDOWN:
-	
-		# Markers passed to watershed algorithm
-		cv2.circle(marker_image,(x,y),10,(current_marker),-1)
-		
-		# Markers visible to user
-		cv2.circle(img_copy,(x,y),10,colors[current_marker],-1)
-		
-		markers_updated = True
-		
-	
+    global markers_updated
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        # Markers passed to watershed algorithm
+        cv2.circle(marker_image,(x,y),10,(current_marker),-1)
+
+        # Markers visible to user
+        cv2.circle(img_copy,(x,y),10,colors[current_marker],-1)
+
+        markers_updated = True
+
+
 def create_rgb(i): # Colors
-	return tuple(np.array(cm.tab10(i)[:3])*255)
+    return tuple(np.array(cm.tab10(i)[:3])*255)
 colors = []
 for i in range(10):
-	colors.append(create_rgb(i)) 
-	
+    colors.append(create_rgb(i))
+
 print("***WATERSHED SEGMENTATION***\n\
 *Assign color with numeric keys (1-9)\n\
 *Click on input image to segment\n\
@@ -55,43 +60,39 @@ print("***WATERSHED SEGMENTATION***\n\
 
 while True:
 
-	cv2.imshow('Watershed Segments', segments)
-	cv2.imshow('Original image', img_copy)
-	cv2.setMouseCallback('Original image', mouse_callback)
-	
-	k = cv2.waitKey(1)
-	
-	if k == 27:	# Escape key
-		break
-	
-	elif k == ord('s'): # Save segmented image with 's'
-		cv2.imwrite(res_s_folder+'output.jpg', segments)
-	
-	elif k == ord('c'): # Reset colors with 'c'
-		img_copy = img.copy()
-		marker_image = np.zeros(img.shape[:2],dtype=np.int32)
-		segments = np.zeros(img.shape,dtype=np.uint8)
-	
-	# Update colors
-	
-	elif k > 0 and chr(k).isdigit(): # choose color
-		current_marker = int(chr(k))
-	
-	# Update markers
-	
-	if markers_updated:
-		marker_image_copy = marker_image.copy()
-		cv2.watershed(img,marker_image_copy)
-		
-		segments = np.zeros(img.shape,dtype=np.uint8)
-		
-		for color_ind in range(n_markers):
-			# Color the segments
-			segments[marker_image_copy==(color_ind)] = colors[color_ind]
+    cv2.imshow('Watershed Segments', segments)
+    cv2.imshow('Original image', img_copy)
+    cv2.setMouseCallback('Original image', mouse_callback)
+
+    k = cv2.waitKey(1)
+
+    if k == 27:    # Escape key
+        break
+
+    elif k == ord('s'): # Save segmented image with 's'
+        cv2.imwrite(res_s_folder+'segmentation_'+str(s)+'_'+load_im, segments)
+        s = s + 1
+
+    elif k == ord('c'): # Reset colors with 'c'
+        img_copy = img.copy()
+        marker_image = np.zeros(img.shape[:2],dtype=np.int32)
+        segments = np.zeros(img.shape,dtype=np.uint8)
+
+    # Update colors
+
+    elif k > 0 and chr(k).isdigit(): # choose color
+        current_marker = int(chr(k))
+
+    # Update markers
+
+    if markers_updated:
+        marker_image_copy = marker_image.copy()
+        cv2.watershed(img,marker_image_copy)
+
+        segments = np.zeros(img.shape,dtype=np.uint8)
+
+        for color_ind in range(n_markers):
+            # Color the segments
+            segments[marker_image_copy==(color_ind)] = colors[color_ind]
 
 cv2.destroyAllWindows()
-
-
-
-
-
